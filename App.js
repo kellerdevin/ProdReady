35d0bbf5ffce30c749f64f52ab49b6b047117e55
Devin Keller <Kellerdevin7@gmail.com>
2018-06-19 23:57:40 -0700

    Clean Up

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React, { Component, } from 'react';
-import { AppRegistry, StyleSheet, Text, TouchableOpacity,View, Image, NavigatorIOS, Button} from 'react-native';
+import { StyleSheet, Text, View, Image, NavigatorIOS, Button} from 'react-native';
 import firebase from 'firebase';
-import { Header, Spinner, StackNav, } from './src/components/common';
+import { Header, Spinner, } from './src/components/common';
 import BlockInfo from './src/components/common/BlockInfo'
 import ProdPageHead from './src/components/common/ProdPageHead'
+import RecentAdditions from './src/components/common/RecentAdditions';
 import LoginForm from './src/components/LoginForm';
 import api from './src/components/api'
 import { createStackNavigator } from 'react-navigation';
@@ -13,54 +14,21 @@ class HomeScreen extends React.Component {
     constructor(props){
     super(props);
     this.state = {
-      blockinfo: '',
-      avatar: '',
-
       prodTitle: '',
       subTitle: '',
       prodimg: '',
-
-      RAP1T: '',
-      RAP1ST: '',
-      RAP1img: '',
-
-      RAP2T: '',
-      RAP2ST: '',
-      RAP2img: '',
-
-      RAP3T: '',
-      RAP3ST: '',
-      RAP3img: '',
-      
     }
   }
-
 
   componentDidMount(){
     api.twoApi().then((res) => {
       this.setState({
-        blockinfo: res.blockinfo,
-        avatar: res.avatar,
         prodTitle: res.prodTitle,
         subTitle: res.subTitle,
         prodimg: res.prodimg,
-
-        RAP1T: res.RAP1T,
-        RAP1ST: res.RAP1ST,
-        RAP1img: res.RAP1img,
-
-        RAP2T: res.RAP2T,
-        RAP2ST: res.RAP2ST,
-        RAP2img: res.RAP2img,
-
-        RAP3T: res.RAP3T,
-        RAP3ST: res.RAP3ST,
-        RAP3img: res.RAP3img,
-
-        Hash: res.Hash
-
       })
     })
+
     .catch((error) => {
       console.error(error)
     })
@@ -93,7 +61,7 @@ class HomeScreen extends React.Component {
           <View style={{ backgroundColor: 'white' }}>
             <ProdPageHead />
             <View style={{ width: 500, height: 5, backgroundColor: 'white' }} />
-            <Text style={[styles.blue, styles.newProd, styles.all]}>NEW PRODUCT</Text>
+            <Text style={[styles.all, styles.newProd, ]}>NEW PRODUCT</Text>
             <Text style={[styles.prodTitle, styles.all]}>{this.state.prodTitle}</Text>
             <Text style={[styles.subTitle, styles.gray, styles.all]}>{this.state.subTitle}</Text>
          <View>
@@ -112,57 +80,7 @@ class HomeScreen extends React.Component {
           />
         </View>
         <View style={{ width: 500, height: 1, backgroundColor: 'gray', }} />
-        <View>
-          <View style={{ width: 500, height: 10, backgroundColor: 'white' }} />
-          <Text style={[styles.recentAdd, styles.bold, styles.all]}>Recent Additions
-          <Text style={[styles.seeAll, styles.blue,]}>                          See All</Text>
-            <View style={{ width: 500, height: 5, backgroundColor: 'white' }} />
-          </Text>
-        </View>
-        <View style={{ flexDirection: "row", }}>
-          <Image
-            style={styles.rA}
-                source={{ url: this.state.RAP1img }}
-          />
-          <Text style={[styles.rat]}>
-                {'\n'} {this.state.RAP1T} {'\n'} <Text style={[styles.rast,]}> 
-                   {this.state.RAP1ST}          <Text style={[styles.views]}>   View    </Text>{'\n'}
-              {'\n'} {'\n'}
-              <View style={{ width: 500, height: 1, backgroundColor: 'grey' }} />
-            </Text>
-          </Text>
-        </View>
-        <View style={{ flexDirection: "row" }}>
-
-          <Image
-            style={styles.rA}
-                source={{ url: this.state.RAP2img }}
-          />
-          <Text style={[styles.rat]}>
-                {'\n'} {this.state.RAP2T}                    {'\n'}
-            <Text style={[styles.rast,]}>
-                  {this.state.RAP2ST}          <Text style={[styles.views]}>   View    </Text>{'\n'}
-              {'\n'} {'\n'}
-              <View style={{ width: 500, height: 1, backgroundColor: 'grey' }} />
-            </Text>
-          </Text>
-        </View>
-        <View style={{ flexDirection: "row" }}>
-
-          <Image
-            style={styles.rA}
-                source={{ url: this.state.RAP3img }}
-          />
-          <Text style={[styles.rat]}>
-                {'\n'}{this.state.RAP3T} {'\n'}
-            <Text style={[styles.rast]}>
-                  {this.state.RAP3ST}        <Text style={[styles.views]}>   View    </Text>{'\n'}
-              {'\n'} {'\n'}
-              <View style={{ width: 500, height: 1, backgroundColor: 'grey' }} />
-            </Text>
-          </Text>
-
-        </View>
+          <RecentAdditions />
 
       </View>
 
@@ -185,53 +103,6 @@ class HomeScreen extends React.Component {
 }
 
 class DetailsScreen extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      prodTitle: '',
-      subTitle: '',
-      prodimg: '',
-
-      Hash: '',
-      toYou: '',
-      FromTrans: '',
-      To: '',
-
-      DateToYou: '',
-      DateTrans: '',
-      DateCreated: '',
-
-      Merchant: '',
-
-    }
-  }
-
-
-  componentDidMount() {
-    api.twoApi().then((res) => {
-      this.setState({
-        prodTitle: res.prodTitle,
-        subTitle: res.subTitle,
-        prodimg: res.prodimg,
-
-        
-        Hash: res.Hash,
-        toYou: res.toYou,
-        FromTrans: res.FromTrans,
-        To: res.To,
-        
-        DateToYou: res.DateToYou,
-        DateTrans: res.DateTrans,
-        DateCreated: res.DateCreated,
-
-        Merchant: res.Merchant,
-
-      })
-    })
-      .catch((error) => {
-        console.error(error)
-      })
-  }
   render() {
     return (
         <BlockInfo />
@@ -244,194 +115,20 @@ const styles = StyleSheet.create({
   all: {
     marginLeft: 20,
   },
-  header: {
-    fontSize: 30,
-    fontWeight: 'bold'
-  },
-  textStyle: {
-    alignSelf: 'center',
-    color: '#007aff',
-    fontSize: 12,
-    fontWeight: '600',
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 24,
-  },
-  bold: {
-    fontWeight: 'bold',
-  },
-  hash: {
-    fontSize: 10,
-  },
-  gray: {
-    color: 'dimgray',
-  },
-  blue: {
-    color: 'blue',
-  },
   newProd: {
     fontSize: 10,
+    color: 'blue'
   },
   prodTitle: {
     fontSize: 23
   },
   subTitle: {
-    fontSize: 18,
-  },
-  recentAdd: {
-    fontSize: 18,
-    paddingLeft: 10,
-  },
-  seeAll: {
-    fontSize: 13,
-    textAlign: 'right',
-    fontWeight: 'normal',
-  },
-  rA: {
-    width: 90,
-    height: 80,
-    paddingBottom: 1,
-  },
-  rat: {
-    fontSize: 16,
-  },
-  rast: {
-    fontSize: 14,
-    color: 'dimgrey',
-  },
-  copy: {
-    color: 'blue',
-    fontSize: 8,
-    backgroundColor: 'whitesmoke',
-    fontWeight: 'bold'
-  },
-  views: {
-    color: 'blue',
-    backgroundColor: 'whitesmoke',
-    textAlign: 'right',
-    fontWeight: 'bold',
-    flex: 1,
-    flexDirection: 'column',
-    margin: 30,
-    marginTop: 560,
-    backgroundColor: 'whitesmoke',
-    minHeight: 60,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 30
-  },
-  bold: {
-    fontWeight: 'bold',
-  },
-  hash: {
-    fontSize: 10,
-  },
-  gray: {
-    color: 'dimgray',
-  },
-  blue: {
-    color: 'blue',
-  },
-  newProd: {
-    fontSize: 10,
-  },
-  prodTitle: {
-    fontSize: 23
-  },
-  subTitle: {
-    fontSize: 18,
-  },
-  recentAdd: {
-    fontSize: 22,
-    paddingLeft: 10,
-  },
-  seeAll: {
-    fontSize: 15,
-    fontWeight: 'normal',
-  },
-  rA: {
-    width: 90,
-    height: 90,
-    paddingBottom: 1,
-  },
-  rat: {
-    fontSize: 16,
-  },
-  rast: {
-    fontSize: 14,
-    color: 'dimgrey',
-  },
-  copy: {
-    color: 'blue',
-    fontSize: 8,
-    backgroundColor: 'whitesmoke',
-    fontWeight: 'bold'
-  },
-  views: {
-    color: 'blue',
-    backgroundColor: 'whitesmoke',
-    fontWeight: 'bold',
-    flex: 1,
-    flexDirection: 'column',
-    margin: 30,
-    marginTop: 560,
-    minHeight: 60,
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderRadius: 30,
-    fontSize: 16,
-  },
-  profile: {
-    width: 70,
-    height: 60,
-  },
-  subtitle: {
-    fontSize: 24,
+    fontSize: 20,
     color: '#8E8B93',
   },
-  transfer: {
-    color: '#4F66EE',
-    fontSize: 18,
-    backgroundColor: 'whitesmoke',
+  gray: {
+    color: 'dimgray',
   },
-  more: {
-    color: '#4467DA',
-    fontSize: 12,
-  },
-  Blue: {
-    color: '#4467DA'
-  },
-  BlockInfo: {
-    fontWeight: 'bold',
-    fontSize: 24,
-  },
-  Hash: {
-    fontSize: 12,
-    fontFamily: 'Apple SD Gothic Neo',
-    color: 'black'
-  },
-  TopText: {
-    fontWeight: 'bold',
-    fontSize: 14,
-    backgroundColor: '#E9E4F1',
-  },
-  BottemText: {
-    fontSize: 12,
-    backgroundColor: '#E9E4F1',
-    shadowColor: 'black',
-  },
-  all: {
-    marginLeft: 20
-  },
-  greyText: {
-    backgroundColor: '#E9E4F1',
-  },
-  date: {
-    color: '#8C8794',
-    fontFamily: 'Apple SD Gothic Neo',
-    fontSize: 15
-  }
-
 
 });
 
---
+++ src/components/common/RecentAdditions.js
@@ -0,0 +1,142 @@
+import React, { Component, } from 'react';
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, View, Image, NavigatorIOS, Button } from 'react-native';
+import api from '../api'
+
+class RecentAdditions extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            RAP1T: '',
+            RAP1ST: '',
+            RAP1img: '',
+            RAP2T: '',
+            RAP2ST: '',
+            RAP2img: '',
+            RAP3T: '',
+            RAP3ST: '',
+            RAP3img: '',
+        }
+    }
+
+    componentDidMount() {
+        api.twoApi().then((res) => {
+            this.setState({
+                RAP1T: res.RAP1T,
+                RAP1ST: res.RAP1ST,
+                RAP1img: res.RAP1img,
+                RAP2T: res.RAP2T,
+                RAP2ST: res.RAP2ST,
+                RAP2img: res.RAP2img,
+                RAP3T: res.RAP3T,
+                RAP3ST: res.RAP3ST,
+                RAP3img: res.RAP3img,
+            })
+        })
+            .catch((error) => {
+                console.error(error)
+            })
+    }
+    render() {
+        return (
+            <View>
+                <View style={{ width: 500, height: 10, backgroundColor: 'white' }} />
+                <Text style={[styles.all, styles.bold, styles.recentAdd ]}>Recent Additions
+                    <Text style={[styles.seeAll, styles.blue,]}>                          See All</Text>
+                    <View style={{ width: 500, height: 5, backgroundColor: 'white' }} />
+                </Text>
+                <View style={{ flexDirection: "row", }}>
+                    <Image
+                        style={styles.rA}
+                        source={{ url: this.state.RAP1img }}
+                    />
+                    <Text style={[styles.rat]}>
+                        {'\n'} {this.state.RAP1T} {'\n'} <Text style={[styles.rast,]}>
+                            {this.state.RAP1ST}          <Text style={[styles.views]}>   View    </Text>{'\n'}
+                            {'\n'} {'\n'}
+                            <View style={{ width: 500, height: 1, backgroundColor: 'grey' }} />
+                        </Text>
+                    </Text>
+                </View>
+                <View style={{ flexDirection: "row" }}>
+
+                    <Image
+                        style={styles.rA}
+                        source={{ url: this.state.RAP2img }}
+                    />
+                    <Text style={[styles.rat]}>
+                        {'\n'} {this.state.RAP2T}                    {'\n'}
+                        <Text style={[styles.rast,]}>
+                            {this.state.RAP2ST}          <Text style={[styles.views]}>   View    </Text>{'\n'}
+                            {'\n'} {'\n'}
+                            <View style={{ width: 500, height: 1, backgroundColor: 'grey' }} />
+                        </Text>
+                    </Text>
+                </View>
+                <View style={{ flexDirection: "row" }}>
+
+                    <Image
+                        style={styles.rA}
+                        source={{ url: this.state.RAP3img }}
+                    />
+                    <Text style={[styles.rat]}>
+                        {'\n'}{this.state.RAP3T} {'\n'}
+                        <Text style={[styles.rast]}>
+                            {this.state.RAP3ST}        <Text style={[styles.views]}>   View    </Text>{'\n'}
+                            {'\n'} {'\n'}
+                            <View style={{ width: 500, height: 1, backgroundColor: 'grey' }} />
+                        </Text>
+                    </Text>
+
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    all: {
+        marginLeft: 20,
+    },
+    bold: {
+        fontWeight: 'bold',
+    },
+    recentAdd: {
+        fontSize: 22,
+        paddingLeft: 10,
+    },
+    seeAll: {
+        fontSize: 15,
+        fontWeight: 'normal',
+    },
+    blue: {
+        color: 'blue',
+    },
+    rA: {
+        width: 90,
+        height: 90,
+        paddingBottom: 1,
+    },
+    rat: {
+        fontSize: 16,
+    },
+    rast: {
+        fontSize: 14,
+        color: 'dimgrey',
+    },
+    views: {
+        color: 'blue',
+        backgroundColor: 'whitesmoke',
+        fontWeight: 'bold',
+        flex: 1,
+        flexDirection: 'column',
+        margin: 30,
+        marginTop: 560,
+        minHeight: 60,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 30,
+        fontSize: 16,
+    },
+})
+
+export default RecentAdditions;
